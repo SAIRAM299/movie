a2f71b2b1dcc76de7fdab46b9a77de5a13349b39
SAIRAM299 <psairam299gmail.com>
2024-01-13 10:55:44 +0530

    oo

--- src/component/About/About.jsx
+++ src/component/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import con from '../../Assets/abk.png'
-import the from '../../Assets/abt.png'
-import com from '../../Assets/abu.png'
+import con from '../../Assets/abo/abk.png'
+import the from '../../Assets/abo/abt.png'
+import com from '../../Assets/abo/abu.png'
 import './About.css'
 const About = () => {
     return (
